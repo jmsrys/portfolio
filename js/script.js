aa609495a928f1344c49d6bb444cbf15e1b4d863
jmsrys <85496617+jmsrys@users.noreply.github.com>
2023-09-01 00:58:44 +0800

    button function, js ++

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
+// NAV-FUNCTION
+
+
+
+
+
+
+// SWIPER
 var swiper = new Swiper(".slide-container", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    centerSlide: "true",
-    grabCursor: "true",
-    fade: "true",
+  slidesPerView: 2,
+  spaceBetween: 30,
+  slidesPerGroup: 2,
+  loop: false,
+  centerSlide: "true",
+  grabCursor: "true",
+  fade: "true",
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -20,14 +28,42 @@ var swiper = new Swiper(".slide-container", {
         0: {
             slidesPerView:1,
             slidesPerGroup: 1,
+            loop: true,
         },
         520: {
             slidesPerView:1,
             slidesPerGroup: 1,
         },
         768: {
-            slidesPerView:3,
+          slidesPerView: 2,
+          spaceBetween: 30,
         },
 
     },
   });
+
+
+// ABOUT-TAB FUNCTION
+  var tablinks = document.getElementsByClassName("tab-links");
+  var tabcontents = document.getElementsByClassName("tab-contents");
+
+  function opentab(tabname){
+      for(tablink of tablinks){
+          tablink.classList.remove("active-link");
+      }
+      for(tabcontent of tabcontents){
+          tabcontent.classList.remove("active-tab");
+      }
+      event.currentTarget.classList.add("active-link");
+      document.getElementById(tabname).classList.add("active-tab");
+  }
+
+
+
+  // EMAIL JS
+ 
+
+
+  
+  
+  
